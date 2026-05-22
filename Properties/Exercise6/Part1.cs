bfee554dc703792744ff8d7d66c3b9e00e86b069
trankhanhhuyencm-ctrl <trankhanhhuyencm@gmail.com>
2026-05-22 22:28:49 +0700

    bổ sung

--- Properties/Exercise6/Part1.cs
+++ Properties/Exercise6/Part1.cs
@@ -17,9 +17,9 @@ namespace CSLT_Solution.Properties.Exercise6
             int[] arr = { 10, 12, 34, 56, 70, 25, 81, 44, 67, 4 };
             //1
             double tb = avg(arr);
-            Console.WriteLine($"Trung bình giá trị của trong mảng: {tb}");
+            Console.WriteLine($"1. Trung bình giá trị của trong mảng: {tb}");
             //2
-            Console.Write("Nhập vào một số để kiểm tra tồn tại: ");
+            Console.Write("2. Nhập vào một số để kiểm tra tồn tại: ");
             int n = int.Parse(Console.ReadLine());
             bool check = kiemtratontai(arr, n);
             if (check)
@@ -27,25 +27,46 @@ namespace CSLT_Solution.Properties.Exercise6
             else
                 Console.WriteLine($"{n} không tồn tại trong mảng");
             //3.
-            Console.Write("Nhập vào một số để kiểm tra vị trí: ");
+            Console.Write("3. Nhập vào một số để kiểm tra vị trí: ");
             int k = int.Parse(Console.ReadLine());
             int vt = TimViTri(arr, k);
             if (vt != -1)
             {
-                Console.WriteLine($"{k} tồn tại trong mảng tại vị trí: {vt}");
+                Console.WriteLine($"{k} tồn tại trong mảng tại vị trí: {vt + 1}");
             }
             else
             {
                 Console.WriteLine($"{k} không tồn tại trong mảng");
             }
+            //4.
+            Console.Write("4. Nhập số phần tử mảng: ");
+            int h = int.Parse(Console.ReadLine());
+            int[] mangBai4 = new int[h];
+            phatsinh_Mang(mangBai4, h);
+            Console.Write("Mảng vừa được phát sinh ngẫu nhiên là: ");
+            In_Mang(mangBai4, h);
+            Console.Write("Nhập phần tử muốn xóa: ");
+            int x = int.Parse(Console.ReadLine());
+            bool kq = XoaPhanTu(mangBai4, ref h, x);
+            if (!kq)
+            {
+                Console.WriteLine($"{x} không tồn tại trong mảng để xóa!");
+            }
+            else
+            {
+                Console.Write($"Mảng sau khi xóa phần tử {x}: ");
+                In_Mang(mangBai4, h); 
+            }
+            Console.ReadLine();
         }
+
         //1.to calculate the average value of array elements.
-        static double avg (int[] arr)
+        static double avg(int[] arr)
         {
             double a = 0, sum = 0;
             foreach (int i in arr)
                 sum += i;
-            a=sum/arr.Length;
+            a = sum / arr.Length;
             return a;
         }
         //2.to test if an array contains a specific value.
@@ -62,24 +83,42 @@ namespace CSLT_Solution.Properties.Exercise6
             for (int i = 0; i < arr.Length; i++)
             {
                 if (arr[i] == k)
-                    return i; 
+                    return i;
             }
             return -1;
+        }
         //4.to remove a specific element from an array.
-        static int [] XoaPhanTu(int[] arr, int n)
+        static bool XoaPhanTu(int[] arr, ref int h, int x)
         {
-            int[] newArr = new int[arr.Length - 1];
-            int index = 0;
-            foreach (int i in arr)
+            for (int i = 0; i < h; i++)
             {
-                if (i != n)
+                if (arr[i] == x)
                 {
-                    newArr[index] = i;
-                    index++;
+                    for (int j = i; j < h - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    h--;
+                    return true;
                 }
             }
-            return newArr;
+            return false;
+        }
+        static void phatsinh_Mang(int[] a, int h)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < h; i++)
+            {
+                a[i] = rand.Next(1, 100);
             }
-
+        }
+        static void In_Mang(int[] a, int h)
+        {
+            for (int i = 0; i < h; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
